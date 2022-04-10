a460c2bc97ee108e3eeceec75270105631656936
Georgesui <gdimov445@gmail.com>
2022-04-10 20:13:44 +0300

    final commit

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ let selectedElements = document.querySelector('#elements');
 const circleTosquare = document.querySelector('#card')
 selectedElements.addEventListener('change', (eventTouch) => {
 	let newValue = eventTouch.target.value;
-	circleTosquare.classList.toggle(newValue)
+	circleTosquare.setAttribute('class', newValue)
+})
+
+let ourColorInput = document.querySelector('#colorValue');
+let buttonForColor = document.querySelector('button');
+if (ourColorInput.value = '#000000') {
+	circleTosquare.style.backgroundColor = '#000000'
+}
+buttonForColor.addEventListener('click', (event) => {
+	circleTosquare.style.backgroundColor = ourColorInput.value;
 })
